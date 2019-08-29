094d574f29272329f5a05a23f04f878502ac3342
dremlin <ykolisnichenko@gmail.com>
2019-08-29 11:29:39 +0300

    New promt

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-'use strict';
-
-
-
-let money = +prompt('Ваш бюджет на месяц?' , '' );
-let time = prompt('Введите дату в формате YYYY-MM-DD' , '' );
-let expensesStateOfOutcomes = prompt('Введите обязательную статью расходов в этом месяце' , '' );
-let sumOfExpenses = +prompt( 'Во сколько обойдется?' , '' );
-
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-    budget: money,
-    timeData: time,
-    expences: {
-        expensesStateOfOutcomes, sumOfExpenses
-              
-    },
-    optionalExpenses:{} ,
-    income: [],
-    savings: false,
-
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-alert((money-sumOfExpenses)/30 );
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
 
 
 
